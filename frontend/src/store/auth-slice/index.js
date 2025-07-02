297f96e1f9f8a63c713ff0e03a26eda3395d3884
Santoshkumar Hadagali <shadagali@grepruby.io>
2025-07-02 17:33:06 +0530

    second commit

--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState={
     isAuthenticated:false,
@@ -6,6 +7,19 @@ const initialState={
     user:null
 }
 
+export const registerUser=createAsyncThunk(
+    "/auth/register",
+    async (formData)=>{
+        const response=await axios.post(
+            "http://localhost:5000/api/auth/register",
+            formData,
+            {
+                withCredentials:true,
+            }
+        );
+        return response.data;
+    }
+)
 
 
 const authSlice=createSlice({
@@ -13,8 +27,20 @@ const authSlice=createSlice({
     initialState,
     reducers:{
         setUser:(state,action)=>{
-
         }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(registerUser.pending, (state)=>{
+            state.isLoading=true
+        }).addCase(registerUser.fulfilled,(state,action)=>{
+            state.isLoading=false;
+            state.user=null;
+            state.isAuthenticated=false;
+        }).addCase(registerUser.rejected,(state,action)=>{
+            state.isLoading=false;
+            state.user=null;
+            state.isAuthenticated=false;
+        })
     }
 })
 
