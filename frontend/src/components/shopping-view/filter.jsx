cb496459ef0e7b96381b81220b61b9794298dcd5
Santoshkumar <santoshkumarbh2001@gmail.com>
2025-07-10 22:59:17 +0530

    day7f

--- frontend/src/components/shopping-view/filter.jsx
+++ frontend/src/components/shopping-view/filter.jsx
@@ -1,6 +1,8 @@
 import { filterOptions } from "@/config";
 import { Fragment } from "react";
 import { Label } from "../ui/label";
+import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
 
 export default function PorductFilter(){
     return(
@@ -15,10 +17,14 @@ export default function PorductFilter(){
                             <h3 className="text-base font-bold">{keyItem}</h3>
                             <div className="grid-gap-2 mt-2">
                                 {
-                                    filterOptions[keyItem].map(option => <Label></Label>)
+                                    filterOptions[keyItem].map(option => <Label className="flex items-center gap-2 font-medium">
+                                        <Checkbox/>
+                                        {option.label}
+                                    </Label>)
                                 }
                             </div>
                         </div>
+                        <Separator/>
                     </Fragment>
                 )))}
             </div>
--- frontend/src/pages/shopping-view/listing.jsx
+++ frontend/src/pages/shopping-view/listing.jsx
@@ -1,9 +1,27 @@
 import PorductFilter from "@/components/shopping-view/filter";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ArrowUpDownIcon } from "lucide-react";
 
 export default function ShoppingListing(){
     return(
         <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
             <PorductFilter/>
-        </div>
+            <div className="bag-background w-full rounded-lg shadow-sm">
+                <div className="p-4 border-b flex item-center justify-between">
+                    <h2 className="text-lg font-extrabold">All Products</h2>
+                    <div className="flex items-center gap-2">
+                        <span className="text-muted-foreground">10 products</span>
+                    </div>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="sm" className="flex items-center gap-1">
+                                <ArrowUpDownIcon className="h-4 w-4"/>
+                            </Button>
+                        </DropdownMenuTrigger>
+                    </DropdownMenu>
+                </div>
+            </div>
+        </div> 
     )
 }
